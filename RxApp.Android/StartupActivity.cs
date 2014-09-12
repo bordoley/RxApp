0740a739018bc07ec35164d9b141fe7354af6ee7
David Bordoley <bordoley@gmail.com>
2014-09-11 22:08:19 -0700

    In non-coldboot case, make sure the splash screen is shown for at least 1 second so the experience isn't jarring.

--- RxApp.Android/StartupActivity.cs
+++ RxApp.Android/StartupActivity.cs
@@ -1,15 +1,33 @@
 ﻿using Android.App;
 using Android.OS;
 using System;
+using System.Threading.Tasks;
 
 namespace RxApp
 {
     public sealed class StartupActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        private static bool isColdBoot = true;
+
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
+            if (isColdBoot)
+            {
+                isColdBoot = false;
+
+            }
+            else
+            {
+                await Task.Delay(new TimeSpan(10000000));
+            }
+
+            StartApp();
+        }
+
+        private void StartApp()
+        { 
             var app = (IRxAndroidApplication) this.Application;
             app.Start();
             this.Finish();
