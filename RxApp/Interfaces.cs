746bf514b8164bb2c0b816350c108149124f091a
Dave <bordoley@gmail.com>
2015-02-22 18:28:19 -0800

    remove out of date usings

--- RxApp/Interfaces.cs
+++ RxApp/Interfaces.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reactive;
-using System.Windows.Input;
 
 namespace RxApp
 {
--- RxApp/MobileModel.cs
+++ RxApp/MobileModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Windows.Input;
 using System.Reactive;
 using System.Reactive.Linq;
 
