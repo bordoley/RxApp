e56e49d1e94700bea7f89d7a4aeeab4e8c38d619
Dave <bordoley@gmail.com>
2015-02-24 21:18:08 -0800

    Add RxFragment that supports activation.

---
+++ RxApp.Android/RxFragment.cs
@@ -0,0 +1,67 @@
+﻿using Fragment = Android.App.Fragment;
+using SupportFragment = Android.Support.V4.App.Fragment;
+
+using RxApp;
+
+namespace RxApp.Android
+{
+    public abstract class RxFragment<TViewModel> : Fragment, IViewFor<TViewModel>
+        where TViewModel : IActivationViewModel
+    {
+        private TViewModel viewModel;
+
+        public TViewModel ViewModel
+        {
+            get { return viewModel; }
+
+            set { viewModel = value; }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return viewModel; }
+
+            set { this.ViewModel = (TViewModel) value; }
+        }
+
+        public override void OnResume()
+        {
+            viewModel.Activate.Execute();
+        }
+
+        public override void OnPause()
+        {
+            viewModel.Deactivate.Execute();
+        }
+    } 
+
+    public abstract class RxSupportFragment<TViewModel> : SupportFragment, IViewFor<TViewModel>
+        where TViewModel : IActivationViewModel
+    {
+        private TViewModel viewModel;
+
+        public TViewModel ViewModel
+        {
+            get { return viewModel; }
+
+            set { viewModel = value; }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return viewModel; }
+
+            set { this.ViewModel = (TViewModel) value; }
+        }
+
+        public override void OnResume()
+        {
+            viewModel.Activate.Execute();
+        }
+
+        public override void OnPause()
+        {
+            viewModel.Deactivate.Execute();
+        }
+    } 
+}
