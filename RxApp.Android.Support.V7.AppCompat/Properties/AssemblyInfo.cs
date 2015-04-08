2ed16c5d97761ea8d21caed7a8f653d5ffb2f6e6
Dave <bordoley@gmail.com>
2015-04-08 12:14:24 -0400

    fix file name casing

--- RxApp.Android.Support.V7.AppCompat/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("RxApp.Android.Support.v7")]
-[assembly: AssemblyDescription("Extends RxApp.Android with features available in the Android Support library V7 AppCompat")]
-[assembly: AssemblyCopyright("")]
---
+++ RxApp.Android.Support.v7.AppCompat/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("RxApp.Android.Support.v7.AppCompat")]
+[assembly: AssemblyDescription("Extends RxApp.Android with features available in the Android Support library v7 AppCompat")]
+[assembly: AssemblyCopyright("")]
